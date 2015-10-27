8130c65d9907f4f6179cdcb26ce66ff604e1679a
Rob Whitaker <robjameswhitaker@gmail.com>
2015-10-26 22:36:56 -0400

    Added alternate audio files. Still uses mp3 as backup because IE literally can't even.

--- src/InteractiveStory.elm
+++ src/InteractiveStory.elm
@@ -15,11 +15,13 @@ import StoryContent
 
 import Howler exposing (emptyAudioObject)
 
+addAudioExtensions file = List.map (\ext -> "assets/audio/" ++ file ++ ext) [".ogg", ".aac", ".mp3"]
+
 app = StartApp.start {
     init = init StoryContent.stuff [
-      ("mansion-bgm", { emptyAudioObject | src <- ["assets/audio/541205_Halloween-Waltz-LOOP.mp3"], html5 <- Just True}),
-      ("portal-bgm", { emptyAudioObject | src <- ["assets/audio/452916_happy_halloween.mp3"], html5 <- Just True, volume <- Just 0.9}),
-      ("trick-or-treat-bgm", { emptyAudioObject | html5 <- Just True, src <- ["assets/audio/563651_KH-Halloween-Town-Remix-2.wav"]})
+      ("mansion-bgm", { emptyAudioObject | src <- addAudioExtensions "541205_Halloween-Waltz-LOOP", html5 <- Just True}),
+      ("portal-bgm", { emptyAudioObject | src <- addAudioExtensions "452916_happy_halloween", html5 <- Just True, volume <- Just 0.9}),
+      ("trick-or-treat-bgm", { emptyAudioObject | html5 <- Just True, src <- addAudioExtensions "563651_KH-Halloween-Town-Remix-2"})
       ],
     view = render,
     update = update,
